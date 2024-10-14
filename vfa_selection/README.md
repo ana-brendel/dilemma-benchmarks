63307df9bbe698478de40b4b7a576f8c848d11b4
Ana Brendel <anabrendel25@gmail.com>
2024-10-13 19:42:29 -0700

    add all of the selection benchmarks

---
+++ vfa_selection/README.md
@@ -0,0 +1,8 @@
+# Verified Functional Algorithms - Selection Chapter
+
+**How to Run**
+1. Install findlemma before running any tests.
+2. `cd common` then `make && make install`
+3. For each test, `cd tests && coqc <test>`
+
+Note there is no script included to test all of the benchmarks in once; currently running individually.
--- vfa_selection/tests/cons_of_small_maintains_sort_by_Permutation_in.v
+++ vfa_selection/tests/cons_of_small_maintains_sort_by_Permutation_in.v
@@ -1,7 +1,7 @@
 (** * Selection:  Selection Sort *)
 (* Some proofs from: https://github.com/kolya-vasiliev/verified-functional-algorithms-2019/blob/master/Selection.v *)
 
-(* From LFindToo Require Import LFindToo. *)
+From LFindToo Require Import LFindToo.
 
 Require Import vfa_selection_benchmarks.Definitions.
 Require Import vfa_selection_benchmarks.Decide.
@@ -209,139 +209,13 @@ Proof.
   (* HELPER LEMMA - case 3 (non-generalized) *)
   eapply le_all__le_one. eauto. 
   (* HELPER LEMMA - case 3 (non-generalized) *)
-  eapply Permutation_in.
+  findlemma. Admitted.
+
+  (* eapply Permutation_in.
   (* HELPER LEMMA - case 3 (non-generalized) *)
   apply Permutation_sym.
   (* HELPER LEMMA - case 3 (non-generalized) *) 
   apply selsort_perm. eauto.
   rewrite <- H. rewrite K. simpl. auto. 
   auto.
-Qed.
-
-Lemma selsort_sorted : forall n al, length al = n -> sorted (selsort al n).
-Proof.
-  intros. generalize dependent al. induction n.
-  - intros. assert (P : al = []). destruct al. auto. simpl in H. inversion H. 
-  rewrite P. simpl. apply sorted_nil.
-  - intros. destruct al. simpl in H. inversion H.
-  simpl in H. inversion H. simpl. pose (select_exists al n0).
-  inversion e. inversion H0.
-  rewrite H2. 
-  (* HELPER LEMMA - case 2 *)
-  apply cons_of_small_maintains_sort.
-  + (* HELPER LEMMA - case 3 (non-generalized) *) 
-    eapply select_rest_length. eauto.
-  + (* HELPER LEMMA - case 3 (non-generalized) *) 
-    eapply select_smallest. eauto.
-  + rewrite H1. apply IHn. rewrite <- H1. symmetry.
-  (* HELPER LEMMA - case 3 (non-generalized) *) 
-    eapply select_rest_length. eauto.
-Qed. 
-
-Lemma selection_sort_sorted : forall al, sorted (selection_sort al).
-Proof.
-    unfold selection_sort. intros.
-    (* HELPER LEMMA - case 2 *)
-    apply selsort_sorted. reflexivity.
-Qed.
-
-(* These are just two lemmas that make up the "is_a_sorting_algorithm" predicate *)
-Theorem selection_sort_is_correct : is_a_sorting_algorithm selection_sort.
-Proof.
-  unfold is_a_sorting_algorithm. intros. split.
-  apply selection_sort_perm. apply selection_sort_sorted.
-Qed.
-
-(* ################################################################# *)
-(** * Recursive Functions That are Not Structurally Recursive *)
-
-Require Import Recdef.  (* needed for [measure] feature *)
-
-Function selsortt l {measure length l} :=
-  match l with
-  | [] => []
-  | x :: r => let (y, r') := select x r
-            in y :: selsortt r'
-end.
-Proof.
-  intros l x r HL y r' Hsel.
-  apply select_rest_length in Hsel. rewrite <- Hsel. simpl. auto.
-Defined.
-
-Lemma selsortt_perm : forall n l, length l = n -> Permutation l (selsortt l).
-Proof.
-  induction n.
-  - intros. rewrite length_zero_iff_nil in H. subst. apply Permutation_refl.
-  - intros. destruct l. inversion H. rewrite selsortt_equation. 
-    destruct (select n0 l) eqn:Seq. apply perm_trans with (n1 :: l0).
-    + pose (select_perm n0 l). rewrite Seq in p. apply p. auto.
-    + apply perm_skip. apply IHn. simpl in H. inversion H. 
-    symmetry. eapply select_rest_length. eauto.
-    (* This is how dude from GitHub did it below *)
-    (* + apply perm_skip. apply IHn. assert (length (n0::l) = (length (n1::l0))).
-    { 
-      apply Permutation_length.
-      pose (select_perm n0 l).
-      rewrite Seq in p.
-      apply p. auto.
-    } inversion H0. inversion H. reflexivity. *)
-  Qed.
-
-Lemma list_has_length {T} (l : list T): exists x, length l = x.
-Proof. induction l. exists 0. reflexivity. inversion IHl. exists (S x). simpl. auto.
-Qed.
-
-Lemma cons_of_small_maintains_sortt: forall bl y,
-    y <=* bl -> sorted (selsortt bl) -> sorted (y :: selsortt bl).
-Proof. 
-  intros. assert (L: exists x, length bl = x). apply list_has_length.
-  inversion L. apply selsortt_perm in H1. induction (selsortt bl).
-  - apply sorted_1.
-  - apply sorted_cons. eapply le_all__le_one. eauto. eapply Permutation_in. 
-  apply Permutation_sym. eauto. simpl. auto. auto.
-Qed.
-
-Lemma selsortt_sorted : forall n al, length al = n -> sorted (selsortt al).
-Proof. 
-  intros. generalize dependent al. induction n.
-  - intros. assert (P : al = []). destruct al. auto. simpl in H. inversion H. 
-  rewrite P. simpl. apply sorted_nil.
-  - intros. destruct al. simpl in H. inversion H.
-  simpl in H. inversion H. simpl. pose (select_exists al n0).
-  inversion e. inversion H0. rewrite selsortt_equation. rewrite H2. apply cons_of_small_maintains_sortt.
-  + eapply select_smallest. eauto.
-  + apply IHn. rewrite <- H1. symmetry. eapply select_rest_length. eauto.
-Qed. 
-
-Theorem selsortt_is_correct :
-  is_a_sorting_algorithm selsortt.
-Proof. 
-  unfold is_a_sorting_algorithm. intros. assert (E: exists x, length al = x). apply list_has_length.
-  inversion E. split.
-  eapply selsortt_perm. eauto.
-  eapply selsortt_sorted. eauto.
-Qed.
-
-(* ################################################################# *)
-(** * Selection Sort with Multisets (Optional) *)
-
-(** This section relies on [Multiset] --> don't have decidability for *)
-
-From VFA Require Import Multiset.
-
-Lemma select_contents : forall al bl x y,
-  select x al = (y, bl) ->
-  union (singleton x) (contents al) = union (singleton y) (contents bl).
-Proof. (* FILL IN HERE *) Admitted.
-
-Lemma selection_sort_contents : forall n l,
-  length l = n ->
-  contents l = contents (selection_sort l).
-Proof. (* FILL IN HERE *) Admitted.
-
-Lemma sorted_iff_sorted : forall l, sorted l <-> Sort.sorted l.
-Proof. (* FILL IN HERE *) Admitted.
-
-Theorem selection_sort_correct' :
-  is_a_sorting_algorithm' selection_sort.
-Proof. (* FILL IN HERE *) Admitted.
+Qed. *)
