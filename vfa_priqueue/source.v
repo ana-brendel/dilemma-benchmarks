dd10647521f328514175fd309ebf8df16c101f8d
Ana Brendel <anabrendel25@gmail.com>
2025-01-21 17:39:06 -0800

    set up priqueue

--- vfa_priqueue/source.v
+++ vfa_priqueue/source.v
@@ -1,67 +1,8 @@
-(** * Priqueue: Priority Queues *)
+Require Import vfa_priqueue_benchmarks.Definitions.
+Require Import vfa_priqueue_benchmarks.Decide. 
 
-(** A _priority queue_ is an abstract data type with the following operations:
-
-  - [ empty: priqueue ]
-  - [ insert: key -> priqueue -> priqueue ]
-  - [ delete_max: priqueue -> option (key * priqueue) ]
-
-  The idea is that you can find (and remove) the highest-priority element.
-  Priority queues have applications in:
-  - Discrete-event simulations: The highest-priority event is the
-     one whose scheduled time is the earliest.  Simulating one event
-     causes new events to be scheduled in the future.
-  - Sorting: _heap sort_ puts all the elements in a priority queue,
-     then removes them one at a time.
-  - Computational geometry:  algorithms such as _convex hull_ use
-     priority queues.
-  - Graph algorithms: Dijkstra's algorithm for finding the shortest path
-     uses a priority queue.
-
-  We will be considering _mergeable_ priority queues, with one
-   additional operator:
-
-  - [ merge: priqueue -> priqueue -> priqueue ]
-
-  The classic data structure for priority queues is the "heap", a balanced
-  binary tree in which the the key at any node is _bigger_ than all the
-  keys in nodes below it.  With heaps, [empty] is constant time,
-  [insert] and [delete_max] are logN time.  But [merge] takes NlogN
-  time, as one must take all the elements out of one queue and
-  insert them into the other queue.
-
-  Another way to do priority queues is by _balanced binary search trees_
-  (such as red-black trees); again, [empty] is constant time,
-  [insert] and [delete_max] are logN time, and [merge] takes NlogN
-  time, as one must take all the elements out of one queue and
-  insert them into the other queue.
-
-  In the _Binom_ chapter we will examine an algorithm in which
-  [empty] is constant time,  [insert], [delete_max], and [merge]
-  are logN time.
-
-  In _this_ chapter we will consider a much simpler (and slower)
-  implementation, using unsorted lists, in which:
-   - [empty] takes constant time
-   - [insert] takes constant time
-   - [delete_max] takes linear time
-   - [merge] takes linear time
-
-*)
-
-(* ################################################################# *)
-(** * Module Signature
-
-    This is the "signature" of a correct implementation of priority queues
- where the keys are natural numbers.   Using [nat] for the key type is
- a bit silly, since the comparison function Nat.ltb takes linear time
- in the value of the numbers!  But you have already seen in the
- [Extract] chapter how to define these kinds of algorithms
- on key types that have efficient comparisons, so in this chapter
- (and the [Binom] chapter) we simply won't worry about the
- time per comparison. *)
-
-From VFA Require Import Perm.
+Require Import Coq.Lists.List.
+Require Import Coq.Sorting.Permutation. 
 
 Module Type PRIQUEUE.
   Parameter priqueue: Type.
@@ -110,21 +51,8 @@ End PRIQUEUE.
 
 Module List_Priqueue <: PRIQUEUE.
 
-(** Now we are responsible for providing [Definitions] of all
-  those [Parameters], and proving [Theorems] for all those [Axioms],
-  so that the values in the [Module] match the types in
-  the [Module Type].   If we try to [End List_Priqueue] before
-  everything is provided, we'll get an error.  Uncomment the next
-  line and try it!
-*)
-
-(* End List_Priqueue. *)
-
 (* ================================================================= *)
 (** ** Some Preliminaries *)
-
-(** A copy of the [select] function from Selection.v, but
-    getting the max element instead of the min element: *)
 
 Fixpoint select (i: nat) (l: list nat) : nat * list nat :=
 match l with
@@ -162,7 +90,6 @@ bdestruct (i >=? a).
 *
 destruct (select i al) eqn:?H.
 (* FILL IN HERE *) Admitted.
-(** [] *)
 
 (* ================================================================= *)
 (** ** The Program *)
@@ -191,8 +118,6 @@ Definition merge (p q: priqueue) : priqueue := p++q.
 
 Definition priq (p: priqueue) := True.
 
-(** The abstraction relation is trivial too. *)
-
 Inductive Abs':  priqueue -> list key -> Prop :=
 Abs_intro: forall p, Abs' p p.
 
@@ -205,11 +130,6 @@ Lemma can_relate : forall p, priq p -> exists al, Abs p al.
 Proof.
   intros. exists p; constructor.
 Qed.
-
-(** When the [Abs] relation says, "priority queue [p] contains elements [al]",
-   it is free to report the elements in any order.  It could even relate [p]
-   to two different lists [al] and [bl], as long as one is a permutation of
-   the other.  *)
 
 Lemma abs_perm: forall p al bl,
    priq p -> Abs p al -> Abs p bl -> Permutation al bl.
@@ -238,8 +158,6 @@ Lemma delete_max_Some_priq:
       forall p q k, priq p -> delete_max p = Some(k,q) -> priq q.
 Proof. constructor. Qed.
 
-(** **** Exercise: 2 stars, standard (simple_priq_proofs) *)
-
 Lemma delete_max_None_relate:
   forall p, priq p ->
       (Abs p nil <-> delete_max p = None).
@@ -266,8 +184,6 @@ Lemma merge_relate:
        Permutation al (pl++ql).
 Proof.
 (* FILL IN HERE *) Admitted.
-(** [] *)
 
 End List_Priqueue.
 
-(* 2024-12-25 17:34 *)
